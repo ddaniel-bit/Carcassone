9c979590262685e8e91c725bb0be32746d7d31a3
mskvszkyt <71025319+mskvszkyt@users.noreply.github.com>
2023-04-12 11:58:31 +0200

    Update script.js

--- carcassone html/script.js
+++ carcassone html/script.js
@@ -1,4 +1,12 @@
-$( '.navbar-nav a' ).on( 'click', function () {
-	$( '.navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-	$( this ).parent( 'li' ).addClass( 'active' );
-});
+var images = ["img/fomenuscreen.png", "img/jatekkezdesscreen.png", "img/palyavalasztas.png","img/beallitasok.png", "img/nemtudszlerakni.png", "img/Pontozas.png", "img/toplista.png" ]; //,"img/tren.png"
+var currentIndex = 0;
+
+function changeImage(n) {
+  currentIndex += n;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  } else if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  document.getElementById("mainImage").src = images[currentIndex];
+}
